507aa9423aec61eaf86468677211b727f782c968
Katherine Champagne <kechampagne@gmail.com>
2013-08-11 17:43:39 -0500

    Fix Tweet attribution typo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,4 @@
 module ApplicationHelper
 	include TweetButton
-	TweetButton.default_tweet_button_options = {:via => "@raplaughs", :count => "none", :text => "#raplaughs"}
+	TweetButton.default_tweet_button_options = {:via => "raplaughs", :count => "none", :text => "#raplaughs"}
 end
